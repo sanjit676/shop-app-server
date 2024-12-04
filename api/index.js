0d9a1cf258aa577866a567e8b458d80b9da5dd98
sanjit961 <sanjitlungeli241@gmail.com>
2024-12-04 14:31:26 +0530

    configured for production

--- api/index.js
+++ api/index.js
@@ -3,20 +3,18 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
-const authRouter = require("./routes/auth/auth-route");
-const adminProductsRouter = require("./routes/admin/products-routes");
-const adminOrderRouter = require("./routes/admin/order-routes");
+const authRouter = require("../routes/auth/auth-route");
+const adminProductsRouter = require("../routes/admin/products-routes");
+const adminOrderRouter = require("../routes/admin/order-routes");
 
-const shopProductsRouter = require("./routes/shop/products-routes");
-const shopCartRouter = require("./routes/shop/cart-routes");
-const shopAddressRouter = require("./routes/shop/address-routes");
-const shopOrderRouter = require("./routes/shop/order-routes");
-const shopSearchRouter = require("./routes/shop/search-routes");
-const shopReviewRouter = require("./routes/shop/review-routes");
+const shopProductsRouter = require("../routes/shop/products-routes");
+const shopCartRouter = require("../routes/shop/cart-routes");
+const shopAddressRouter = require("../routes/shop/address-routes");
+const shopOrderRouter = require("../routes/shop/order-routes");
+const shopSearchRouter = require("../routes/shop/search-routes");
+const shopReviewRouter = require("../routes/shop/review-routes");
 
-const commonFeatureRouter = require("./routes/common/feature-routes");
-
-
+const commonFeatureRouter = require("../routes/common/feature-routes");
 
 const PORT = process.env.PORT || 5000;
 
@@ -62,3 +60,5 @@ app.use("/api/common/feature", commonFeatureRouter);
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
 });
+
+module.exports = app;
